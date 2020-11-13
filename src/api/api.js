5b24a17d0c02a00644bb3122d696169f712ecd60
Martin Osvald <m.osvald9913@gmail.com>
2020-11-13 23:02:16 +0100

    added methot and axios Instance

---
+++ src/api/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { endpoints, baseUrl, headers } from "./apiConstants";
+
+const axiosInstance = axios.create({
+  baseUrl: baseUrl,
+  headers: headers.basic,
+});
+
+export const api = {
+  getFestival: () =>
+    axiosInstance
+      .get(endpoints.festival)
+      .then((res) => res.data)
+      .catch((err) => console.error(err)),
+  getInterpret: () =>
+    axiosInstance
+      .get(endpoints.interpret)
+      .then((res) => res.data)
+      .catch((err) => console.error(err)),
+  getReservation: () =>
+    axiosInstance
+      .get(endpoints.reservation)
+      .then((res) => res.data)
+      .catch((err) => console.error(err)),
+  getStage: () =>
+    axiosInstance
+      .get(endpoints.stage)
+      .then((res) => res.data)
+      .catch((err) => console.error(err)),
+};
--- src/api/apiConstants.js
+++ src/api/apiConstants.js
@@ -1,10 +1,16 @@
-const baseUrl = "https://localhost:44354/api/";
+export const baseUrl = "https://localhost:44354/api/";
 
-const endpoints = {
+export const endpoints = {
   festival: "Festival",
   interpret: "Interpret",
   reservation: "Reservation",
   stage: "Stage",
   user: "User",
   userAuthenticate: "User/authenticate/",
+};
+
+export const headers = {
+  basic: {
+    "Content-Type": "application/octet-stream",
+  },
 };
