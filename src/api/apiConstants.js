ec4941f332f93bb8ee52d25ba3b7dffb14ec3f6f
Martin Osvald <m.osvald9913@gmail.com>
2020-11-13 22:44:12 +0100

    add axios , add constants

---
+++ src/api/apiConstants.js
@@ -0,0 +1,10 @@
+const baseUrl = "https://localhost:44354/api/";
+
+const endpoints = {
+  festival: "Festival",
+  interpret: "Interpret",
+  reservation: "Reservation",
+  stage: "Stage",
+  user: "User",
+  userAuthenticate: "User/authenticate/",
+};
