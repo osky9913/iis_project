4007d27796f8c11a9e09cfac988c5110653dd6a2
Martin Osvald <m.osvald9913@gmail.com>
2020-11-20 22:11:30 +0100

    fixed name-ing

--- src/components/layout/AppBar/CustomAppBar.jsx
+++ src/components/layout/AppBar/CustomAppBar.jsx
@@ -7,6 +7,7 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
 import LoginDialog from "./LoginDialog/LoginDialog";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,6 +36,14 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6" className={classes.title}>
             Festivaly
           </Typography>
